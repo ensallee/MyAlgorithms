1ead024516bc97be1598c3030e970b23ea06218f
ensallee <ensallee@gmail.com>
2018-08-14 18:07:02 -0400

    steps

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ function capitalize(str) {
     return cap += word.slice(1)
   }).join(" ")
 }
+
+//Form a visual staircase of n height by console.logging out steps with #.
+//There should be spaces to the right of each #.
+//Example: steps(3)
+//       '#  '
+//       '## '
+//       '###'
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
