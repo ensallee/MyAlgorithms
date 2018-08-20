964b4c5458bb887c33ebd9da95b4bf58c26e12d1
ensallee <ensallee@gmail.com>
2018-08-20 13:35:53 -0400

    fizzbuzz

--- index.js
+++ index.js
@@ -55,6 +55,25 @@ function maxChar(str) {
   return maxChar;
 }
 
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz")
+    } else if (i % 3 === 0) {
+      console.log("fizz")
+    } else if (i % 5 === 0) {
+      console.log("buzz")
+    } else {
+      console.log(i)
+    }
+  }
+}
+
 //Capitalize the first letter of each word in a string.
 function capitalize(str) {
   return str.split(" ").map(word => {
